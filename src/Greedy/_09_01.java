97772f9f71101f6a529f611993c51b0ff7530ab5
opixxx <opix0306@naver.com>
2023-07-05 17:14:37 +0900

    씨름선수

---
+++ src/Greedy/_09_01.java
@@ -0,0 +1,47 @@
+package Greedy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Body implements Comparable<Body> {
+    public int h, w;
+    Body(int h, int w) {
+        this.h = h;
+        this.w = w;
+    }
+    @Override
+    public int compareTo(Body o) {
+        return o.h - this.h;
+
+    }
+
+}
+
+public class _09_01 {
+    public int solution(ArrayList<Body> arr) {
+        int answer = 0;
+        Collections.sort(arr);
+        int max = Integer.MIN_VALUE;
+        for (Body ob : arr) {
+            if (ob.w > max) {
+                answer++;
+                max = ob.w;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        _09_01 T = new _09_01();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Body> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            arr.add(new Body(h, w));
+        }
+        System.out.println(T.solution(arr));
+
+    }
+}
