323a99a3933032a2e95369c5e132d364a770703b
opixxx <opix0306@naver.com>
2023-07-13 20:49:50 +0900

    프로그래머스 해쉬

--- src/Programmers/hash_4.java
+++ src/Programmers/hash_4.java
@@ -3,24 +3,21 @@ package Programmers;
 import java.util.*;
 class hash_4 {
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer;
         int a = 1;
         HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
         }
         ArrayList<Integer> arr = new ArrayList<>();
-        if(map.size() == 1){
-            answer = clothes.length;
-        } else {
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                arr.add(entry.getValue());
-            }
-            for (int i = 0; i < arr.size(); i++) {
-                a *= arr.get(i) + 1;
-            }
-            answer = a - 1;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            arr.add(entry.getValue());
         }
+        for (int x : arr) {
+            a *= x + 1;
+        }
+        answer = a - 1;
+
         return answer;
     }
 }
