c5089dcea5468e85c646e48bf97b0eadd8bdb4d7
opixxx <opix0306@naver.com>
2023-07-13 16:56:55 +0900

    동전교환 (냅색 알고리즘)

---
+++ src/Dynamic/_10_05.java
@@ -0,0 +1,40 @@
+package Dynamic;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _10_05 {
+    static int[] coin;
+    static int m, n;
+    static int[] dy;
+    public int solution(int[] coin) {
+        Arrays.fill(dy, Integer.MAX_VALUE);
+        dy[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= m; j++) {
+                dy[j] = Math.min(dy[j], dy[j- coin[i]] + 1);
+                // int tmp = dy[j - coin[i]] + 1;
+                // if (tmp < dy[j]) {
+                //    dy[j] = tmp;
+                // }
+            }
+        }
+        return dy[m];
+
+
+    }
+    public static void main(String[] args) {
+        _10_05 T = new _10_05();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        coin = new int[n];
+        for (int i = 0; i < n; i++) {
+            coin[i] = sc.nextInt();
+        }
+        m = sc.nextInt();
+        dy = new int[m + 1];
+        System.out.println(T.solution(coin));
+
+
+    }
+}
