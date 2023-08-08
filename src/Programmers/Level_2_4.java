99d8f87a747bcf53b8971d0e3d2074ff60e55450
opixxx <opix0306@naver.com>
2023-08-08 16:06:14 +0900

    다음 큰 수 (bitCount 사용 시 더 쉬움)

--- src/Programmers/Level_2_4.java
+++ src/Programmers/Level_2_4.java
@@ -1,2 +1,28 @@
-package Programmers;public class Level_2_4 {
+package Programmers;
+
+class Level_2_4 {
+    public int solution(int n) {
+        int answer = 0;
+        int cnt = 0;
+        String a = Integer.toBinaryString(n);
+        for(int i = 0; i < a.length(); i++) {
+            if(a.charAt(i) == '1') cnt++;
+        }
+        while(true) {
+            int count = 0;
+            n++;
+            String tmp = Integer.toBinaryString(n);
+            for(int i = 0; i < tmp.length(); i++) {
+                if(tmp.charAt(i) == '1') count++;
+            }
+            if(cnt == count) {
+                answer = n;
+                break;
+            }
+            // Integer.bitCount() 사용하면 더 간결한 코드 가능
+
+
+        }
+        return answer;
+    }
 }
