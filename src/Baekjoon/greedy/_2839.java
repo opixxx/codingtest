7854b01cd69677e1806e64e314b6f974a0769424
opixxx <opix0306@naver.com>
2023-11-09 18:00:20 +0900

    백준 2839 설탕 배달

---
+++ src/Baekjoon/greedy/_2839.java
@@ -0,0 +1,24 @@
+package Baekjoon.greedy;
+
+import java.util.Scanner;
+
+public class _2839 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while (true) {
+            if (n % 5 == 0) {
+                System.out.println(n / 5 + cnt);
+                break;
+
+            } else if (n < 0) {
+                System.out.println(-1);
+                break;
+            }
+            n = n - 3;
+            cnt++;
+        }
+    }
+
+}
