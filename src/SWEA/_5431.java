59b02159c4ff772dd122d9d2a7903607ad7be727
opixxx <opix0306@naver.com>
2023-11-16 19:09:31 +0900

    SWEA 5431 민석이의 과제 체크하기

---
+++ src/SWEA/_5431.java
@@ -0,0 +1,30 @@
+package SWEA;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class _5431 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+
+        for (int tc = 1; tc <= test; tc++) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n + 1];
+            for (int i = 0; i < k; i++) {
+                int person = sc.nextInt();
+                arr[person] = 1; //과제 한사람
+            }
+
+            System.out.print("#" + tc + " ");
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] == 0) {
+                    System.out.print(i+ " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
