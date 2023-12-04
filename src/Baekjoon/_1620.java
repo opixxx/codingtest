5bbbd82abc303ead418e90d46b6e5a75f37db512
opixxx <opix0306@naver.com>
2023-12-04 23:26:24 +0900

    백준 1620 나는야 포켓몬 마스터 이다솜

--- src/Baekjoon/_1620.java
+++ src/Baekjoon/_1620.java
@@ -1,25 +1,29 @@
 package Baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class _1620 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder answer = new StringBuilder();
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         HashMap<String, Integer> map = new HashMap<>();
         String[] arr = new String[n + 1];
         for (int i = 1; i <= n; i++) {
-            arr[i] = sc.next();
+            arr[i] = br.readLine();
             map.put(arr[i], i);
         }
 
         for (int i = 0; i < m; i++) {
-            String str = sc.next();
+            String str = br.readLine();
             if (isStringNumber(str)) {
                 int index = Integer.parseInt(str);
                 answer.append(arr[index]);
