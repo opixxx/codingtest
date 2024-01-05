6400969168552aba17cd1905553bf5db334601dd
opixxx <opix0306@naver.com>
2024-01-05 17:20:17 +0900

    백준 14405 피카츄

---
+++ src/Baekjoon/_14405.java
@@ -0,0 +1,50 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class _14405 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String answer = "NO";
+        String input = sc.next();
+        int check = 0;
+        // pi, ka, chu
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 'p') {
+                if (i + 2 > input.length()) {
+                    break;
+                }
+                String tmp = input.substring(i, i + 2);
+                if (tmp.equals("pi")) {
+                    check += 2;
+                }
+
+            }
+            if (input.charAt(i) == 'c') {
+                if (i + 3 > input.length()) {
+                    break;
+                }
+                String tmp = input.substring(i, i + 3);
+                if (tmp.equals("chu")) {
+                    check += 3;
+                }
+            }
+            if (input.charAt(i) == 'k') {
+                if (i + 2 > input.length()) {
+                    break;
+                }
+                String tmp = input.substring(i, i + 2);
+                if (tmp.equals("ka")) {
+                    check += 2;
+                }
+            }
+            if (check == input.length()) break;
+        }
+        if (check == input.length()) {
+            answer = "YES";
+        }
+
+        System.out.println(answer);
+
+    }
+}
