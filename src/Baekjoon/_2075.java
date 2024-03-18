99709a984a13513700653d5fd348a120a5daaa6d
opixxx <opix0306@naver.com>
2024-03-18 21:52:07 +0900

    백준 2075 N 번째 큰수

--- src/Baekjoon/_2075.java
+++ src/Baekjoon/_2075.java
@@ -1,16 +1,25 @@
 package Baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+
 
 public class _2075 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int n = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < n * n; i++) {
-            pq.add(sc.nextInt());
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                pq.add(Integer.parseInt(input[j]));
+            }
         }
         for (int i = 0; i < n - 1; i++) {
             pq.poll();
