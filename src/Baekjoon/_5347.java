22ea3b460e8782028b7cdf59730dfdd1521059f3
opixxx <opix0306@naver.com>
2024-03-19 20:41:18 +0900

    백준 5347 LCM

---
+++ src/Baekjoon/_5347.java
@@ -0,0 +1,36 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _5347 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            long a = Integer.parseInt(st.nextToken());
+            long b = Integer.parseInt(st.nextToken());
+
+            System.out.println(lcm(a, b));
+
+        }
+    }
+
+    private static long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
