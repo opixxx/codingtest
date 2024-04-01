eb5db262730a475d13e605b10f5371369cb5b3a7
opixxx <opix0306@naver.com>
2024-04-01 21:58:02 +0900

    백준 2164 카드2

---
+++ src/Baekjoon/dataStructure/_2164.java
@@ -0,0 +1,24 @@
+package Baekjoon.dataStructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class _2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.add(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            q.add(q.poll());
+        }
+        System.out.println(q.poll());
+    }
+}
