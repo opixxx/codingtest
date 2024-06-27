6fe51f2c63f1f0a267e1a540f86c5ce46ba3a108
opixxx <opix0306@naver.com>
2024-06-27 23:28:55 +0900

    백준 2579 계단 오르기

---
+++ src/Baekjoon/_2579.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2579 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[301];
+        int[] score = new int[301];
+
+        for (int i = 1; i <= n; i++) {
+            score[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = score[1];
+        dp[2] = score[1] + score[2];
+        dp[3] = Math.max(score[1] + score[3], score[2] + score[3]);
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 3] + score[i - 1], dp[i - 2]) + score[i];
+        }
+
+        System.out.println(dp[n]);
+
+
+    }
+}
