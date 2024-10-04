d07524ea3ee1c4856af8c3ded637906118baf44d
opixxx <opix0306@naver.com>
2024-10-04 22:30:58 +0900

    백준 12865 평범한 배낭

---
+++ src/Baekjoon/_12865.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _12865 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[w + 1];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+            for (int j = w; j >= weight; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight] + value);
+            }
+
+        }
+        System.out.println(dp[w]);
+
+    }
+}
