891470f77444a552df210f696b934fdbfe75e649
opixxx <opix0306@naver.com>
2024-11-17 23:55:41 +0900

    백준 25635 자유 이용권

--- src/Baekjoon/_25635.java
+++ src/Baekjoon/_25635.java
@@ -3,6 +3,7 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _25635 {
@@ -16,8 +17,9 @@ public class _25635 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Long.parseLong(st.nextToken());
-
         }
+
+        Arrays.sort(arr);
 
         for (int i = 0; i < n - 1; i++) {
             sum += arr[i];
