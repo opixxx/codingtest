8bd62407bf429e11cefd664cb53494254d32e13f
opixxx <opix0306@naver.com>
2024-11-24 18:14:19 +0900

    백준 23843 콘센트

---
+++ src/Baekjoon/_23843.java
@@ -0,0 +1,46 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class _23843 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < m; i++) {
+            pq.offer(0);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int time = pq.poll() + arr[i];
+            pq.offer(time);
+        }
+
+        int answer = 0;
+        while (!pq.isEmpty()) {
+            answer = Math.max(answer, pq.poll());
+        }
+        System.out.println(answer);
+
+
+
+
+    }
+}
