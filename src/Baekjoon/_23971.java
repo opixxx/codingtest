91df7b8f584046a02a8e7b2f7c173b93100ef97e
opixxx <opix0306@naver.com>
2024-12-20 03:43:31 +0900

    백준 23971 ZOAC 4

---
+++ src/Baekjoon/_23971.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _23971 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int a = (h - 1) / (n + 1) + 1;
+        int b = (w - 1) / (m + 1) + 1;
+
+        System.out.println(a * b);
+    }
+}
