93116ca54270c687fed0a5f2106280eec9dec60a
opixxx <opix0306@naver.com>
2024-12-31 18:59:48 +0900

    프로그래머스 점프와 순간 이동

---
+++ src/Programmers/점프와_순간_이동.java
@@ -0,0 +1,18 @@
+package Programmers;
+
+public class 점프와_순간_이동 {
+    public int solution(int n) {
+        int ans = 0;
+
+        while(n != 0) {
+
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
