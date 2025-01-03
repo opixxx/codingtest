9c5367c0ea58b3520f510c4ce8dc5fbe478292c3
opixxx <opix0306@naver.com>
2025-01-03 21:52:09 +0900

    프로그래머스 예상 대진표

---
+++ src/Programmers/예상_대진표.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+public class 예상_대진표 {
+    public int solution(int n, int a, int b) {
+        int answer = 1;
+        while (true) {
+
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+
+            if (a == b) break;
+
+            answer++;
+        }
+        return answer;
+    }
+}
