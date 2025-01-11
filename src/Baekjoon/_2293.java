43fb023bf6673693813788035e01506ceec41e82
opixxx <opix0306@naver.com>
2025-01-11 22:32:14 +0900

    백준 2293 동전 1

---
+++ src/Baekjoon/_2293.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2293 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[n];
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = dp[j] + dp[j - coin[i]];
+            }
+        }
+
+        System.out.print(dp[k]);
+
+
+    }
+}
